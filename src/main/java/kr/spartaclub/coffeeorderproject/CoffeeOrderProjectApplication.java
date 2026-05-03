2175668fe26c33a156a2c914a1e680d2142f1514
sohyun kim <sohyun050282@gmail.com>
2026-05-03 19:08:08 +0900

    first commit

---
+++ src/main/java/kr/spartaclub/coffeeorderproject/CoffeeOrderProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.coffeeorderproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOrderProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/spartaclub/coffeeorderproject/CoffeeOrderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.coffeeorderproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOrderProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
